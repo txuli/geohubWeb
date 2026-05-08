7a7710765c7e9185d434553dd6e7166bdd54bd76
txuli <127785824+txuli@users.noreply.github.com>
2026-05-08 19:26:54 +0200

    Add robots.txt and sitemap.xml

--- src/components/header.vue
+++ src/components/header.vue
@@ -4,9 +4,7 @@
 <template>
   <header class=" w-full">
         <nav class="h-12 flex justify-between  ">
-            <h1 class=" my-auto text-2xl text-accent ml-5 font-serif">
-                GeoHub
-            </h1>
+            <img src="/geohub-logo.svg" alt="GeoHub" class="my-auto ml-5 h-11 w-auto" />
             <div class="my-auto">
                     <a class="my-auto mx-5 font-syne text-[#4C515E] hover:text-white transition-all hover:cursor-pointer z-10" href="#live-explorer">Explore</a>
                     <a class="my-auto mx-5 font-syne text-[#4C515E] hover:text-white transition-all hover:cursor-pointer z-10" href="#endpoints">Docs</a>
